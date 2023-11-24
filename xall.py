cfbb1d3e3956576ad53cb01cc956b4e6938598c6
Eero Viittala <eero.viittala@nokia.com>
2023-11-24 09:35:18 +0200

    Adding gzip support

--- xall.py
+++ xall.py
@@ -4,12 +4,18 @@ import re
 import tarfile
 import zipfile
 import argparse
+import shutil
+import gzip
 
 paths = []
 
+def is_gzipfile(filename):
+    tail = getFileTail(filename)
+    return tail == "gz"
+
 def isCompressedFile(filename):
     try:
-        return tarfile.is_tarfile(filename) or zipfile.is_zipfile(filename)
+        return tarfile.is_tarfile(filename) or zipfile.is_zipfile(filename) or is_gzipfile(filename)
     except:
         return False
 
@@ -18,19 +24,36 @@ def getFileHead(filename):
     m = pattern.split(filename)
     return m[0]
 
+def getFileTail(filename):
+    pattern = re.compile(r'\.')
+    m = pattern.split(filename)
+    return m[-1]
+
 def extractFile(folder, filename):
     print("{0} ... ".format(filename), end='')
     head = folder + "/" + getFileHead(filename)
     file = folder + "/" + filename
     try:
         if tarfile.is_tarfile(file):
-            os.mkdir(head)
+            try:
+                os.mkdir(head)
+            except FileExistsError:
+                pass
             tar = tarfile.open(file)
             tar.extractall(head)
             tar.close()
         elif zipfile.is_zipfile(file):
             with zipfile.ZipFile(file) as myzip:
                 myzip.extractall(head)
+        elif is_gzipfile(file):
+            try:
+                os.mkdir(head)
+            except FileExistsError:
+                pass
+            with gzip.open(file, 'rb') as f_in:
+                # TODO filenamewithout extension
+                with open(head + "/" + getFileHead(filename), "wb") as f_out:
+                    shutil.copyfileobj(f_in, f_out)
     except:
         print("nok")
         return False
@@ -63,7 +86,7 @@ def goThroughAllPaths():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog=sys.argv[0], description='Extracts all compressed files')
     parser.add_argument('filename', help='Filename to compress')
-    parser.add_argument('-v', '--version', help='Version of this file', action='version', version='%(prog)s 1.1')
+    parser.add_argument('-v', '--version', help='Version of this file', action='version', version='%(prog)s 1.2')
     args = parser.parse_args()
     if isCompressedFile(args.filename):
         xfile(".", args.filename)
